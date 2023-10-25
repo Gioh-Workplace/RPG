3af3262760e42b2f45673b8e49617116797be76f
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2023-10-25 00:51:44 -0300

    Classe Usuario

---
+++ RPG-Apache-Maven/src/main/java/br/com/rpg/model/Usuario.java
@@ -0,0 +1,37 @@
+package br.com.rpg.model;
+
+public class Usuario {
+    private String username;
+    private String email;
+    private String senha;
+
+    public Usuario(String username, String email, String senha) {
+        this.username = username;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
