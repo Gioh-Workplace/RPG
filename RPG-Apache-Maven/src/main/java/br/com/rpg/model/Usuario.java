df1e88a1a69f588b6ae8b20fd85484e846dd5b39
Cauã <caualeu2016@gmail.com>
2023-11-16 02:41:40 -0300

    Update: Usuario

--- RPG-Apache-Maven/src/main/java/br/com/rpg/model/Usuario.java
+++ RPG-Apache-Maven/src/main/java/br/com/rpg/model/Usuario.java
@@ -1,15 +1,31 @@
 package br.com.rpg.model;
 
 public class Usuario {
+
+    private int id;
     private String username;
     private String email;
     private String senha;
+
+    private int pontuacao;
+
+    public Usuario() {
+    }
+
+    public Usuario(int id, String username, String email, String senha, int pontuacao) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.senha = senha;
+        this.pontuacao = pontuacao;
+    }
 
     public Usuario(String username, String email, String senha) {
         this.username = username;
         this.email = email;
         this.senha = senha;
     }
+
 
     public Usuario(String username) {
         this.username = username;
@@ -37,5 +53,21 @@ public class Usuario {
 
     public void setSenha(String senha) {
         this.senha = senha;
+    }
+
+    public int getPontuacao() {
+        return pontuacao;
+    }
+
+    public void setPontuacao(int pontuacao) {
+        this.pontuacao = pontuacao;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }
